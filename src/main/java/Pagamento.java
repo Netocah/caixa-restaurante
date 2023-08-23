cd7b275cb02e645f4a9bdecd6161963986a6b312
Neto <franciscomdlneto@gmail.com>
2023-08-23 20:30:38 -0300

    Criando as classses

---
+++ src/main/java/Pagamento.java
@@ -0,0 +1,11 @@
+public class FormaPagamento {
+    private String forma;
+
+    public String getForma() {
+        return forma;
+    }
+
+    public void setForma(String forma) {
+        this.forma = forma;
+    }
+}
